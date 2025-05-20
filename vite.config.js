53dd1160e9c189ba2f23376f303c7373a080982e
luisax20 <luifermessi23@gmail.com>
2025-05-20 12:39:05 -0500

    Visualizando css diseño

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,11 @@ export default defineConfig({
                 'resources/css/index.css',
                 'resources/css/login.css',
                 'resources/css/dashboard.css',
-                'resources/css/evaluacion.css',
+                'resources/css/eva_oficial.css',
                 'resources/css/form.css',
                 'resources/css/form_tecnico.css',
                 'resources/css/form_metodologia.css',
                 'resources/css/terms.css',
-                
                 'resources/js/app.js'
             ],
             refresh: true,
